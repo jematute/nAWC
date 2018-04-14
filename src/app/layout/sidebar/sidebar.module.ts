0bd7be58858f466d233245fd2bfaf6e137b7767a
Juan Matute <juan.matute@gmail.com>
2018-04-14 10:57:30 -0400

    paging.

--- src/app/layout/sidebar/sidebar.module.ts
+++ src/app/layout/sidebar/sidebar.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar.component';
 import { SearchModule } from '../../search/search.module';
 import { ColumnsModule } from '../../columns/columns.module';
+import { MaterialModule } from '../../material/material.module';
 
 @NgModule({
   imports: [
-    CommonModule, SearchModule, ColumnsModule
+    CommonModule, SearchModule, ColumnsModule, MaterialModule
   ],
   declarations: [ SidebarComponent ],
   exports: [ SidebarComponent ],
--- src/app/results/grid/grid.component.ts
+++ src/app/results/grid/grid.component.ts
@@ -1,8 +1,9 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit, ViewChild } from '@angular/core';
 import { GridOptions, GridApi, ColumnApi } from 'ag-grid';
 import { GridService } from './grid.service';
 import { GetDataParams, SortDirection, ResultType, AdeptDataTable } from '../../classes/getDataParams';
 import { ColumnsService } from '../../columns/columns.service';
+import { MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-grid',
@@ -11,10 +12,11 @@ import { ColumnsService } from '../../columns/columns.service';
 })
 export class GridComponent implements OnInit {
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   private gridOptions: GridOptions;
   private api: GridApi;
   private columnApi: ColumnApi;
-  private numberOfPages: number;
+  private length: number;
   private pageSize: number;
   private pageIndex: number;
 
@@ -22,34 +24,29 @@ export class GridComponent implements OnInit {
   rowData;
   constructor(private gridService: GridService, private columnService: ColumnsService) {
     this.pageSize = 100;
-    this.numberOfPages = 0;
+    this.length = 0;
 
     this.gridOptions = < GridOptions > {};
     this.gridOptions.rowData = [];
     this.columnDefs = [];
-    this.columnService.getGridColumns().subscribe(cols => {
-      cols.forEach(col => this.columnDefs.push(
-          { 
-            headerName: col.displayName, 
-            field: col.schemaID, 
-            width: col.width*5
-          })
-        );
-        this.gridOptions.api.setColumnDefs(this.columnDefs);
-    });
+    this.gridOptions.rowSelection = 'multiple';
+    this.getColumns();
 
     this.gridService.change.subscribe(res => {
+      this.length = 0;
+      this.pageIndex = 0;
+      this.paginator.firstPage();
       this.getPage(0);
     });
   }
 
-  getPage(pageIndex: number, itemsPerPage?: number) {
+  getPage(pageIndex: number) {
     let params = <GetDataParams>{};
     let AdeptDataTable = <AdeptDataTable>{};
     params.AdeptDataTable = AdeptDataTable;
     params.AdeptDataTable.Skip = pageIndex * this.pageSize;
     params.AdeptDataTable.Take = this.pageSize;
-    params.AdeptDataTable.RecordCount = 0;
+    params.AdeptDataTable.RecordCount = length;
     params.ResultType = ResultType.Normal;
     params.Sort = "SCHEMA_S_LOGNNAME";
     params.SortDirection = SortDirection.Ascending;
@@ -60,9 +57,21 @@ export class GridComponent implements OnInit {
     })
     this.gridService.getCount(params).subscribe(data => {
       let dataTable = data as AdeptDataTable;
-      this.numberOfPages = Math.ceil(dataTable.RecordCount/this.pageSize);
+      this.length = dataTable.RecordCount
     })
+  }
 
+  getColumns() {
+    this.columnService.getGridColumns().subscribe(cols => {
+      cols.forEach(col => this.columnDefs.push(
+          { 
+            headerName: col.displayName, 
+            field: col.schemaID, 
+            width: col.width*5
+          })
+        );
+        this.gridOptions.api.setColumnDefs(this.columnDefs);
+    });
   }
 
   onReady(event) {
@@ -71,11 +80,14 @@ export class GridComponent implements OnInit {
   }
 
   onPageEvent(event) {
-    console.log(event);
+    console.log("event", event);
+    console.log("current page size", this.pageSize);
+    this.pageSize = event.pageSize;
+    this.getPage(event.pageIndex);
   }
 
   ngOnInit() {
-
+    console.log(this.paginator);
   }
 
 }
